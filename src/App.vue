4cc211f23d7b4c9142ee0a803bc5bdefc8baf6f4
HANATANI Takuma <mail@potato4d.me>
2019-03-30 21:43:44 +0900

    Add sample code

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,85 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button type="button" @click="handleClickButton1">
+      Promise.resolve()
+    </button>
+    <button type="button" @click="handleClickButton2">
+      普通にエラーをなげる
+    </button>
+    <button type="button" @click="handleClickButton3">
+      try でキャッチしてしまう
+    </button>
+    <div style="margin-top: 16px;">
+      Log: <br>
+      <code>
+        <pre>{{log}}</pre>
+      </code>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { inspect } from 'util'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      log: null
+    }
+  },
+  methods: {
+    async handleClickButton1() {
+      const asyncFunc = async () => {
+        const asyncFunc2 = async () => {
+          return 1
+        }
+        return asyncFunc2()
+      }
+      try {
+        await asyncFunc()
+        this.log += "\n" + '最後まで疎通' + "\n----------"
+      } catch(e) {
+        console.log(e)
+        this.log += "\n" + `catch でハンドリング: (${inspect(e)})` + "\n----------"
+      }
+    },
+    async handleClickButton2() {
+      const asyncFunc = async () => {
+        const asyncFunc2 = async () => {
+          throw new Error('何かしらのエラー')
+        }
+        return asyncFunc2().catch((err) => {
+          throw new Error('何かしらのエラー')
+        })
+      }
+      try {
+        await asyncFunc()
+        this.log += "\n" + '最後まで疎通' + "\n"
+      } catch(e) {
+        console.log(e)
+        this.log += "\n" + `catch でハンドリング: (${inspect(e)})` + "\n----------"
+      }
+    },
+    async handleClickButton3() {
+      const asyncFunc = async () => {
+        const asyncFunc2 = async () => {
+          throw new Error('何かしらのエラー')
+        }
+        return asyncFunc2().catch((err) => {
+          throw new Error('何かしらのエラー')
+        })
+      }
+      try {
+        await asyncFunc().catch((e) => {
+          this.log += "\n" + `エラーを途中でキャッチ${inspect(e)}`
+        })
+        this.log += "\n" + '最後まで疎通' + "\n"
+      } catch(e) {
+        console.log(e)
+        this.log += "\n" + `catch でハンドリング: (${inspect(e)})` + "\n----------"
+      }
+    }
   }
 }
 </script>
@@ -21,8 +89,14 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  padding: 16px;
+}
+
+pre {
+  background: #f0f0f0;
+  padding: 0 8px;
+  line-height: 2.0;
+  border-radius: 2px;
 }
 </style>
